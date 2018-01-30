290f6dddffe3b6b7b43d6c82ae31a06d7c713216
samuelsedwin <samuelsedwin.js@gmail.com>
2018-01-30 23:53:45 +0200

    javascript teachings added to Test

--- oz-workspace/test/TestTransfers.js
+++ oz-workspace/test/TestTransfers.js
@@ -1,26 +1,32 @@
 var controller = artifacts.require("Controller");
 
 contract('controller',function(accounts) {
-    it ("should have a water limit of 80",function(){
-        return controller.deployed().then(function(instance){
-             instance.generateToken();
-             instance.setUserWaterLimit(80);
-             return instance.getUserWaterLimit();
-        }).then(function(waterLimit) {
-            assert.equal(waterLimit.valueOf(),80,"80 wasn't the water limit")
-
-        });
+   let control
+    beforeEach(async () => {
+        control = await controller.new()
+    })
+    
+    it ("should have a water limit of 80", async () => {
+        let set
+        let get
+        await control.generateToken();
+        set = await control.setUserWaterLimit(80, {from: accounts[0]});
+        get = await control.getUserWaterLimit.call();
+        assert.equal(80,get.toNumber(),"80 wasn't the water limit")
     });
 
-    it ("should add user", function(){
-        return controller.deployed().then(function(instance){
-        instance.generateToken();
-        instance.addUser(accounts[0]);
-        return instance.containsUser(accounts[0]);
-        }).then(function(user) {
-            assert.equal(user,true,"User not added")
-        });
-    });
+    // it ("should add user", function(){
+    //     return control.deployed().then(function(instance){
+    //     instance.generateToken();
+    //     instance.addUser(accounts[0]);
+        
+    //     return instance.containsUser(accounts[0]);
+    //     }).then(function(user) {
+    //         assert.equal(user,true,"User not added");
+           
+
+    //     });
+    // });
 
 
 });
