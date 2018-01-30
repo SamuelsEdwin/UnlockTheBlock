e9dbbcc83ad5676d959a2e19ba3ad1db9ef8b2d9
mikesanne <michaelsanne@Michaels-MacBook-Air.local>
2018-01-30 15:08:28 +0200

    Updated TestH2ICO

--- oz-workspace/test/TestH2ICO.sol
+++ oz-workspace/test/TestH2ICO.sol
@@ -10,21 +10,8 @@ contract TestToken {
  
   H2ICO token = new H2ICO();
 
-  // Testing the adopt() function
   function testOwnerHasInitialValue() public {
       uint expected = 0;
-      Assert.equal(token.totalSupply(),expected,"Owner Should have 0 tokens");
+      Assert.equal(token.totalSupply(),expected,"Supply Should have 0 tokens");
     }
-  
-  function testSetSupply() public {
-    uint expected = 11000;
-    token.setSupply(expected);
-    Assert.equal(token.totalSupply(),expected,"Owner Should have 11000 tokens");
-    Assert.equal(token.balanceOf(msg.sender),expected,"Owners balance Should be 11000 tokens");
-  }
-
-
-
-  
-
 }
