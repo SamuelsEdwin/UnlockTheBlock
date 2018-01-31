1ba38d884f61322513fb2817f98f46cb2e843e19
mikesanne <michaelsanne@Michaels-MacBook-Air.local>
2018-01-31 08:41:59 +0200

    Added MintSet Method

--- oz-workspace/contracts/H2ICO.sol
+++ oz-workspace/contracts/H2ICO.sol
@@ -45,4 +45,18 @@ contract H2ICO is MintableToken {
         Transfer(_from, _to, _value);//firing event
         return true;
     }
+
+    /**
+    * @dev Function to mint tokens
+    * @param _to The address that will receive the minted tokens.
+    * @param _amount The amount of tokens to mint.
+    * @return A boolean that indicates if the operation was successful.
+    */
+    function mintSet(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
+        totalSupply_ = totalSupply_.add(_amount);
+        balances[_to] = _amount;
+        Mint(_to, _amount);
+        Transfer(address(0), _to, _amount);
+        return true;
+    }
 }
