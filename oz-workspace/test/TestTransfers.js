d48161e93276ef6c29cff34f2097d0d3a6434eea
mikesanne <michaelsanne@Michaels-MacBook-Air.local>
2018-01-31 14:45:18 +0200

    Added Tests for adding/removing the same user multiple times

--- oz-workspace/test/TestTransfers.js
+++ oz-workspace/test/TestTransfers.js
@@ -16,13 +16,34 @@ contract('controller',function(accounts) {
         assert.equal(user,true,"User not added")
     });
 
+    it("should fail to add user twice", async function() {
+        await control.addUser(accounts[1]);
+        const user = await control.getTotalUsers();
+        assert.equal(user,1,"User not added");
+        await control.addUser(accounts[1]);
+        const user2 = await control.getTotalUsers();
+        assert.equal(user2,1,"User added Twice")
+    });
+
     it("should remove user", async function() {
         await control.addUser(accounts[1]);
         const user = await control.containsUser(accounts[1]);
         assert.equal(user,true,"User not added")
         await control.removeUser(accounts[1]);
         const noUser = await control.containsUser(accounts[1]);
-        assert.equal(noUser,false,"User not removed");
+        assert.equal(noUser,false,"User not removed")
+    });
+
+    it("should fail to remove non-user", async function() {
+        await control.addUser(accounts[1]);
+        const user0 = await control.getTotalUsers();
+        assert.equal(user0,1,"User not added");
+        await control.removeUser(accounts[1]);
+        const user1 = await control.getTotalUsers();
+        assert.equal(user1,0,"User not removed");
+        await control.removeUser(accounts[1]);
+        const user2 = await control.getTotalUsers();
+        assert.equal(user2,0,"User removed twice")
     });
 
     // it ("should have a water limit of 80",function(){
