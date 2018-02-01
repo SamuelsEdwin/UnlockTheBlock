80555ac4abcef36b5eca0740ebede20a50bcb904
r3abow95 <reaboa95@gmail.com>
2018-02-01 06:35:48 +0200

    comments

--- oz-workspace/contracts/TokenSeller.sol
+++ oz-workspace/contracts/TokenSeller.sol
@@ -18,12 +18,12 @@ contract TokenSeller is Ownable {
     event MakerWithdrewEther(uint256 ethers);       //event for when seller account withdraws ether from transaction
     event TakerBoughtAsset(address indexed buyer, uint256 ethersSent, uint256 ethersReturned, uint256 tokensBought);    //event for when buyer account purchases token asset
 
-    /** Constructor Method
-     * Constructor should only be called by the TokenSellerFactory contract
-     * @param _asset address of token asset to be sold for ether
-     * @param _sellPrice  Price sales price of lot 
-     * @param _units size of lot 
-     * @param _sellsTokens validate that TokenSeller contract has permission to sell from Factory
+    /** @dev Constructor Method
+     *       Constructor should only be called by the TokenSellerFactory contract
+     * @param _asset address address of token asset to be sold for ether
+     * @param     _sellPrice uint    Price sales price of lot 
+     * @param         _units uint    size of lot 
+     * @param   _sellsTokens bool    validate that TokenSeller contract has permission to sell from Factory
      */
     function TokenSeller(address _asset, uint _sellPrice, uint _units, bool _sellsTokens) public {
         asset = _asset;
@@ -33,9 +33,9 @@ contract TokenSeller is Ownable {
         ActivatedEvent(sellsTokens);
     }
 
-    /** Activate/Deactivate contract sell status
-     * Make of the contract can control sell permission of the contract
-     * @param _sellsTokens permission variable
+    /** @dev Activate/Deactivate contract sell status
+     *       Maker of the contract can control sell permission of the contract
+     * @param _sellsTokens bool permission variable
      */
     function activate(bool _sellsTokens) public onlyOwner {
         sellsTokens = _sellsTokens;
@@ -43,10 +43,10 @@ contract TokenSeller is Ownable {
     }
 
     
-    /** Withdraw tokens from contract
-     * Owner can withdraw tokens deposited into contract.
-     * @param tokens amount of tokens to be withdrawn
-     * @return ok withdrawel confirmation
+    /** @dev Withdraw tokens from contract
+     *       Owner can withdraw tokens deposited into contract.
+     * @param tokens uint amount of tokens to be withdrawn
+     * @return    ok bool withdrawel confirmation
      */
     function makerWithdrawTokens(uint256 tokens) public onlyOwner returns (bool ok) {
         require(StandardToken(asset).balanceOf(this) >= tokens);
@@ -54,30 +54,30 @@ contract TokenSeller is Ownable {
         return StandardToken(asset).transfer(owner, tokens);
     }
     
-    /** Token balance of contract
-     * @return balance shows the balance of tokens deposited in contract
+    /** @dev Token balance of contract
+     * @return balance uint Shows the balance of tokens deposited in contract
      */
     function makerGetBalance() public view returns (uint balance) {
         return StandardToken(asset).balanceOf(this);
     }
  
-    /** Return Tokens to Contract Maker
-     * Used to return tokens accidentally sent for sale, that is, if the incorrect token type was sent.
-     * Only permissable for contract owner to perform reversal
-     * @param tokenAddress address of token accidentally sent
-     * @param amount amount sent to be reversed
-     * @return confirm confirmation to ensure function executed
+    /** @dev Return Tokens to Contract Maker
+     *       Used to return tokens accidentally sent for sale, that is, if the incorrect token type was sent.
+     *       Only permissable for contract owner to perform reversal
+     * @param tokenAddress address Address of token accidentally sent
+     * @param       amount    uint amount sent to be reversed
+     * @return     confirm    bool confirmation to ensure function executed
      */
     function makerWithdrawERC20Token(address tokenAddress, uint amount) public onlyOwner returns (bool confirm) {
         MakerWithdrewERC20Token(tokenAddress, amount);
         return StandardToken(tokenAddress).transfer(owner, amount);
     }
 
-    /** Withdraw Ether from Contract
-     * Maker can withdraw ether sent to contract for trade. Only can be executed by contract owner.
-     * Contract balance is checked to ensure sufficient ether is available for withdrawel.
-     * @param amount ether amount to be withdrawn
-     * @return confirm transaction confirmation
+    /** @dev Withdraw Ether from Contract
+     *       Maker can withdraw ether sent to contract for trade. Only can be executed by contract owner.
+     *       Contract balance is checked to ensure sufficient ether is available for withdrawel.
+     * @param   amount uint ether amount to be withdrawn
+     * @return confirm bool transaction confirmation
      */
     function makerWithdrawEther(uint amount) public onlyOwner returns (bool confirm) {
         
@@ -88,16 +88,16 @@ contract TokenSeller is Ownable {
         return false;
     }
 
-    /** Show ether balance for contract
-     * @return ether shows ether held in smart contract
+    /** @dev Show ether balance for contract
+     * @return ether uint shows ether held in smart contract
      */
     function getEtherBalance() public view returns (uint ethers) {
         return this.balance;
     }
 
-    /** Taker Purchases Token
-     * Tokens held in contract are purchased in ether. The contract is held and owned by the maker.
-     * The taker pays ether to the contract in exchange for the deposited tokens
+    /** @dev Taker Purchases Token
+     *       Tokens held in contract are purchased in ether. The contract is held and owned by the maker.
+     *       The taker pays ether to the contract in exchange for the deposited tokens
      */
     function takerBuyAsset() public payable {
         if (sellsTokens || msg.sender == owner) {
