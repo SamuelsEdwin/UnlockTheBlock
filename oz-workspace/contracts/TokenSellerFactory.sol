47052c417ad875ad0eb1358c5221151b2408dccd
r3abow95 <reaboa95@gmail.com>
2018-02-01 06:21:00 +0200

    minor change

--- oz-workspace/contracts/TokenSellerFactory.sol
+++ oz-workspace/contracts/TokenSellerFactory.sol
@@ -23,7 +23,7 @@ contract TokenSellerFactory is Ownable {
     event TradeListing(address indexed ownerAddress, address indexed tokenSellerAddress,address indexed asset, uint256 sellPrice, uint256 units, bool sellsTokens);
     event OwnerWithdrewERC20Token(address indexed tokenAddress, uint256 tokens);
     address public TOKEN_ADDRESS;
-    mapping (address => bool) _verify;      //mapping to verify address has a smart contract made by factory
+    mapping (address => bool) public _verify;      //mapping to verify address has a smart contract made by factory
     
     /** @dev Constructor method
      *  @param _tokenAddress address Address of the token to be generated for contracts 
@@ -68,8 +68,8 @@ contract TokenSellerFactory is Ownable {
      * @param sellsTokens check if the contract has permission to sell
      */
     function createSaleContract(uint sellPrice, uint units, bool sellsTokens) public returns (address seller) {
-        require(sellPrice >= 0);
-        require(units >= 0);
+        require(sellPrice >= 0 && units >= 0);
+        //require(units >= 0);
         seller = new TokenSeller(TOKEN_ADDRESS, sellPrice, units, sellsTokens);
         _verify[seller] = true;
         TokenSeller(seller).transferOwnership(msg.sender);
